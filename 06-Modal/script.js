8630e51841f992a9ed5dcb812c084d1ab1822a37
Robert Young <david41596@yahoo.com>
2022-03-23 19:48:37 -0400

    Modal created

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,19 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const toggleHidden = () => {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', toggleHidden);
+}
+closeButton.addEventListener('click', toggleHidden);
+document.addEventListener('keydown', e => {
+  console.log(modal.classList.contains('hidden'));
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    toggleHidden();
+  }
+});
